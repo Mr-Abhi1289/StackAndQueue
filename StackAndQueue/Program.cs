8198603ff18e6e87aa7caf2d06581a05d09523a3
unknown <abhimvj575@gmail.com>
2022-12-19 17:28:48 +0530

    [Abhilash] Added: Ability to create a Stack of 56->30->70

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.Display();
+        }
+    }
+}
