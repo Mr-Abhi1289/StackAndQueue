a8906aca9dbdc4f7c4f891861421d9adaca3acc4
unknown <abhimvj575@gmail.com>
2022-12-19 19:22:30 +0530

    [Abhilash] Added: to create a Queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,15 +6,22 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
+           
             Stack stack = new Stack();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
             stack.Display();
             stack.Peek();
             stack.Pop();
             stack.IsEmpty();
             stack.Display();
+
+            Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
         }
     }
 }
